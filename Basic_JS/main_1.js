2ee1c0a4d3754ef6bd3cf23892750a2dd135112d
ekamleen <ekamleenm@gmail.com>
2024-07-02 12:02:15 -0700

    basic js

---
+++ Basic_JS/main_1.js
@@ -0,0 +1,17 @@
+let a = 1
+a = 2 
+console.log(a);
+
+var b = 3 
+b = 3
+console.log(b);
+
+// const c = 9
+// c = 0
+// console.log(c);
+
+let firstname = "ekam";
+let age = 22;
+let isMarried = false;
+
+console.log("this person's name is " + firstname + " and their age is " + age + " and they are married? "  +isMarried )
