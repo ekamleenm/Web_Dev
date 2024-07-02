0ad55a398088668dfbbc3e13959f32e0e1978c2c
ekamleen <ekamleenm@gmail.com>
2024-07-02 12:05:35 -0700

    if/else

--- Basic_JS/main_1.js
+++ Basic_JS/main_1.js
@@ -14,4 +14,12 @@ let firstname = "ekam";
 let age = 22;
 let isMarried = false;
 
-console.log("this person's name is " + firstname + " and their age is " + age + " and they are married? "  +isMarried )
+console.log("this person's name is " + firstname + " and their age is " + age)
+
+// if else statements 
+
+if(isMarried==true){
+    console.log(firstname + " is married")
+}else{
+    console.log(firstname +  " is not married")
+}
