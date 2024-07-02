0af6bb57cc71029f3b35a3b5a54e9ff01c3d2c6b
ekamleen <ekamleenm@gmail.com>
2024-07-02 16:40:40 -0700

    js fund

---
+++ Basic_js_APIs/main_1.js
@@ -0,0 +1,9 @@
+let firstname = "this is a good thing to do"; 
+console.log(firstname.indexOf("thing"));
+let str = "     this";
+console.log(str.indexOf("this"));
+
+
+// we want to slice from 0th index to 5-1th index
+let my_str = "hellokkk";
+console.log(my_str.slice(0,4));
