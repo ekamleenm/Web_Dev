8fdbe418ea583c0b1f9dcd99706300a3b88ade75
ekamleen <ekamleenm@gmail.com>
2024-07-02 13:03:52 -0700

    dictionaries kinda

--- Basic_JS/main_3.js
+++ Basic_JS/main_3.js
@@ -11,3 +11,40 @@ for(let i = 0 ; i < ages.length; i++){
         console.log(ages[i])
     }
 }
+
+
+//  Objects
+
+const personArray = ['a','b','c'];
+const genderArray= ['m','f','m'];
+
+for(let i = 0; i < personArray.length;i++){
+    if (genderArray[i] == 'm'){
+        console.log(personArray[i]);
+    }
+}
+
+//  instead of this above things we can make objects 
+
+//  this is like a dictionary in python
+const user1 = {
+    firstname: "ah",
+    gender: "male"
+}
+ 
+console.log(user1['firstname'])
+ 
+console.log(user1['gender'])
+
+//  array of dictionaries 
+
+const allUsers = [{firstname:"shaina", gender:"female"},
+                {firstname:"ekam", gender:"female"},
+                {firstname:"Gurunaj", gender:"male"}]
+
+//  accessing the above 
+for (let i =0; i < allUsers.length; i ++){
+    if(allUsers[i]["gender"] == 'female'){
+        console.log(allUsers[i]["firstname"] + " -> thats a good match")
+    }
+}
