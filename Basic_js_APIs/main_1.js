f1356ec482ec48fcc7ad4399940463a914e919ea
ekamleen <ekamleenm@gmail.com>
2024-07-02 16:51:57 -0700

    utility func in JS

--- Basic_js_APIs/main_1.js
+++ Basic_js_APIs/main_1.js
@@ -7,3 +7,16 @@ console.log(str.indexOf("this"));
 // we want to slice from 0th index to 5-1th index
 let my_str = "hellokkk";
 console.log(my_str.slice(0,4));
+
+//  Replace  string utility 
+//  These are just temporary replacements not the actual memory is being accessed and modified
+// i think it makes a copy and then replaces it, here is the example -> 
+const string_2 = "hello world this is good";
+console.log(string_2.replace("world","Universe"));
+console.log(string_2.replace("wo","totoro"));
+
+//  this will split and store it in an array 
+const str_3  = "hi this is a web dev course";
+const words = str_3.split(" ")
+console.log(words)
+
