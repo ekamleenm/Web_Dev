d5febffec7591672f6ee0188f6c58f1023f893e1
ekamleen <ekamleenm@gmail.com>
2024-07-02 12:17:06 -0700

    arrays

--- Basic_JS/main_3.js
+++ Basic_JS/main_3.js
@@ -1,1 +1,13 @@
 // Arrays
+const people = ['ekam','shaina','gurnaj','gurupdesh'];
+console.log(people)
+console.log(people[0])
+console.log(people[1])
+
+const ages = [10,12,31,11,56,44,9]
+
+for(let i = 0 ; i < ages.length; i++){
+    if(ages[i]%2==0){
+        console.log(ages[i])
+    }
+}
