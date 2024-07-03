a2acfaf6ec668f6e848f1b9cdc265fa7b92ad672
ekamleen <ekamleenm@gmail.com>
2024-07-02 17:04:42 -0700

    some array funcs

---
+++ Basic_js_APIs/main_2.js
@@ -0,0 +1,15 @@
+// Some array utitlities 
+
+const initialArray = [1,3,4,5,7];
+initialArray.push(56);
+console.log(initialArray)
+// this pops from the behind of the array
+initialArray.pop();
+
+// if you want to pop from front 
+initialArray.shift();
+console.log(initialArray)
+
+// if i wanna put something in front of the array 
+initialArray.unshift(32);
+console.log(initialArray);
