bab9012072e74fde9017c17499cc165fd50c358d
ekamleen <ekamleenm@gmail.com>
2024-07-05 11:33:50 -0700

    Promises Workflow

--- intermediate_js/main_3.js
+++ intermediate_js/main_3.js
@@ -1,4 +1,5 @@
 // Promises : to get rid of callbacks 
+// Promises syncronously return the promise, it doesn't return result immediately but it doesn't return the promise.
 const fs = require("fs");
 function ekam_readFile(){
     return new Promise(function(resolve,reject){
