69f5ebb6a460f053373aa3214eaf8262f3664805
ekamleen <ekamleenm@gmail.com>
2024-07-05 11:27:29 -0700

    Promise Functions

--- intermediate_js/main_3.js
+++ intermediate_js/main_3.js
@@ -1,3 +1,22 @@
 // Promises : to get rid of callbacks 
 const fs = require("fs");
+function ekam_readFile(){
+    return new Promise(function(resolve,reject){
 
+        fs.readFile("a1.txt","utf-8",function(err,data) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+function when_done(data){
+    console.log(data);
+}
+function if_fails(){
+    console.log("file not found");
+}
+ekam_readFile().then(when_done).catch(if_fails)
