72605741d3d38d244bfdcb5ab61b73318c61c106
ekamleen <ekamleenm@gmail.com>
2024-07-06 12:05:40 -0700

    Promises Revised

---
+++ Week_2/Video_1/Revision_2.js
@@ -0,0 +1,23 @@
+// Promises : 
+
+const fs = require("fs");
+
+function read_write(){
+    return new Promise(function(resolve,reject){
+        fs.readFile("a1.txt","utf-8",function(err,data) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+function when_done(data){
+    console.log(data);
+}
+function if_fails(){
+    console.log("file not found");
+}
+read_write().then(when_done).catch(if_fails)
